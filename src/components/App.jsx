e86935218306fdc9fc968f1a4f9a76c7033f6454
YulyaVlasenko <correspondenceoff@gmail.com>
2023-08-16 08:52:33 +0300

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,76 @@
-export const App = () => {
+
+import React, { useEffect, useState } from "react";
+import ContactForm from "./ContactForm/ContactForm";
+import { Section } from "./Section/Section";
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
+import toast, { Toaster } from 'react-hot-toast';
+
+const App = () => {
+  const [contacts, setContacts] = useState(null);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const savedList = localStorage.getItem('contacts');
+    if (savedList) {
+      setContacts(JSON.parse(savedList));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+      if (contacts.length > 0) {
+        toast.success('Contact operation successful!');
+      }
+    }
+  }, [contacts]);
+
+  const createContact = (contact) => {
+    setContacts((prevContacts) => {
+      if (prevContacts === null) {
+        return [contact];
+      } else {
+        return [...prevContacts, contact];
+      }
+    });
+  };
+
+  const handleChangeFilter = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const filteredContacts = contacts
+    ? contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : [];
+
+  const deleteContact = (contactForDelete) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== contactForDelete)
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title='Phonebook'>
+        <ContactForm createContact={createContact} contacts={contacts} />
+      </Section>
+      
+      <Section title='Contacts'>
+        <Filter
+          title='Find contacts by name'
+          handleChangeFilter={handleChangeFilter}
+          value={filter}
+        />
+        {filteredContacts.length > 0 && (
+          <ContactList contacts={filteredContacts} deleteContact={deleteContact} />
+        )}
+      </Section>
+      <Toaster />
+    </>
   );
 };
+
+export default App;
