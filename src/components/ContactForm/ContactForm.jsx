05a9a33a728e064a3b8f45ab461320a08b48f735
YulyaVlasenko <correspondenceoff@gmail.com>
2023-08-16 08:58:37 +0300

    ref

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,74 +1,80 @@
-import { nanoid } from "nanoid"
-import { Component } from "react"
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import { toast } from "react-hot-toast";
+import { nanoid } from "nanoid";
 
-class ContactForm extends Component {
+const ContactForm = ({ createContact, contacts }) => {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
 
-    state = {
-        name: '',
-        number: '',
-    }
-
-    handleChange = ({target: {name, value}}) => {
-        this.setState({
-            [name]: value,
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        if (name === "name") {
+            setName(value);
+        } else if (name === "number") {
+            setNumber(value);
         }
-        )
     }
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        const { name, number } = this.state
-        const { createContact, contacts } = this.props
-        const isExisting = contacts && contacts.find(contact => contact.name === name) 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        const isExisting = contacts && contacts.find(contact => contact.name === name);
         if (isExisting) {
-            toast.error(`${name} is already in contacts.`)
-           
-            return
+            toast.error(`${name} is already in contacts.`);
+            return;
         }
+        
         const contact = {
             name,
             number,
             id: nanoid(),
-        }
-        createContact(contact)
-        this.reset()
+        };
+        createContact(contact);
+        reset();
     }
 
-    reset = () => {
-        this.setState({ number: '', name: '' })
+    const reset = () => {
+        setName('');
+        setNumber('');
     }
-     
-    render() {
-        const {name, number} = this.state
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label htmlFor="#">Name </label>
-                <input type="text" name="name"
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    onChange={this.handleChange} value={name} required  />
-                
-                <label htmlFor="#">Number</label>
-                <input type="tel" name="number"
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    onChange={this.handleChange} value={number} required />
-                
-                <button type="submit">Add contact</button>
-            </form>
-    )}
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name</label>
+            <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces."
+                onChange={handleChange}
+                value={name}
+                required
+            />
+
+            <label htmlFor="number">Number</label>
+            <input
+                type="tel"
+                name="number"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                onChange={handleChange}
+                value={number}
+                required
+            />
+
+            <button type="submit">Add contact</button>
+        </form>
+    );
 }
-
-export default ContactForm
-
 
 ContactForm.propTypes = {
     contacts: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    number: PropTypes.string.isRequired,
-  })),
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+    })),
     createContact: PropTypes.func.isRequired,
-}
+};
+
+export default ContactForm;
